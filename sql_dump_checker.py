089efddf71f1ce2216e5c37fb87beba0fcee574b
mman21 <mman21@student.aau.dk>
2022-02-28 13:28:27 +0100

    Works on SQL dump with base64 encoding

--- sql_dump_checker.py
+++ sql_dump_checker.py
@@ -1,7 +1,23 @@
 import re
+import string
+import base64
+
 
 def sql_dump_checker(file_location):
-    file = open(file_location, r)
-    lines = file.readLines()
+    file = open(file_location, 'r')
+    lines = file.readlines()
 
     for line in lines:
+        pattern = re.compile('[^a-zA-Z0-9+=]')
+        alnum = pattern.sub('',line)     
+        try:
+            decoded = str(base64.b64decode(alnum))
+            if "canary" in decoded:
+                print("Canary detected:")
+                print(decoded)
+        except:
+            continue
+
+if __name__ == "__main__":
+    file_location = input("FIle Path:\n")
+    sql_dump_checker(file_location)
