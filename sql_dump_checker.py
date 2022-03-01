f306a7f65bf080e613056cff04c4688f9505083f
mman21 <mman21@student.aau.dk>
2022-03-01 09:07:58 +0100

    Added print statements

--- sql_dump_checker.py
+++ sql_dump_checker.py
@@ -9,12 +9,17 @@ import sys
 def sql_dump_checker(file_location, string_to_search):
     file = open(file_location, 'r')
     lines = file.readlines()
-
+    print("Searching for: ", string_to_search)
+    
+    # Boolean to see if anything is found
+    found = 0
+    
     for line in lines:
     # Checks the line to see if canary is written in plain text
-        if "canary" in line:
+        if string_to_search in line:
             print("Canary detected:")
             print(line)
+            found += 1
 
         # Regex for characers found in base64
         pattern = re.compile('[^a-zA-Z0-9+=]')
@@ -26,8 +31,13 @@ def sql_dump_checker(file_location, string_to_search):
             if string_to_search in decoded:
                 print("Canary detected:")
                 print(decoded)
+                found += 1
         except:
             continue
+
+    # If nothing is found
+    if found == 0:
+        print("No results for: ", string_to_search)
 
 if __name__ == "__main__":
     file_location = sys.argv[2]
