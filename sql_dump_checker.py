19dbf5d870a975b1297f152930cef1817fd4e792
mman21 <mman21@student.aau.dk>
2022-03-01 08:54:14 +0100

    Ready to be integrated with master_script.py

--- sql_dump_checker.py
+++ sql_dump_checker.py
@@ -1,11 +1,12 @@
 import re
 import string
 import base64
+import sys
 
 # The sql dump can either contain the domain canarytokens.com in plain text or encoded in base64
 # This function will check every line and look for 'canary'. 
 # It also tries to decode every line in case the line is base64 encoded
-def sql_dump_checker(file_location):
+def sql_dump_checker(file_location, string_to_search):
     file = open(file_location, 'r')
     lines = file.readlines()
 
@@ -22,12 +23,16 @@ def sql_dump_checker(file_location):
         # Tries to decode the line, to see if the line is base64 encoded
         try:
             decoded = str(base64.b64decode(alnum))
-            if "canary" in decoded:
+            if string_to_search in decoded:
                 print("Canary detected:")
                 print(decoded)
         except:
             continue
 
 if __name__ == "__main__":
-    file_location = input("File Path:\n")
-    sql_dump_checker(file_location)
+    file_location = sys.argv[2]
+    if len(sys.argv) != 5:
+        print("Please enter string to search")
+        exit(1)
+    string_to_search = sys.argv[4]
+    sql_dump_checker(file_location, string_to_search)
