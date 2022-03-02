b990879fbaef396c8461b8a46362c20d5c9f830a
simortus <simortusos@gmail.com>
2022-03-02 21:45:35 +0100

    wg.py: <---strategy---> inspects the wireguard file to find the endpoint ip address. Then check if it is related to the target domain of a honey-token. <---strategy---> -open and read file. -process the data to get endpoint ip. -strip the string from additional text (https; : ; // ; port-number) -do dns lookup on the ip. -print if something found.
    kube.py:
    <---strategy---> check kubernetes conf file for the server address.
    Same approach as the wg.py. <---strategy--->
    
    main_script.py:
    -changed the match to statement to a if elif tree :). Sorry for your eyes David :p
    -changed subprocess calls to a function, to avoid duplicate code.
    - added def to inspect if it's a wireguard-conf file, since sometimes the file have no ".conf" (to the best of my knowledge, on mac).
     <---strategy--->
     look for keywords["[Interface]", "PrivateKey", "Address","[Peer]", "PublicKey", "AllowedIPs", "Endpoint", "PersistentKeepalive"]
    <---strategy--->
    -added def to inspect if it's a kubernetes-conf file, since sometimes the file have no ".conf". Same strategy as wireguard-conf file.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # canarytokens_finger_printer
-initial_commit
+![alt text](https://1.bp.blogspot.com/-C7apBekpBpU/YQ0cDZp_YaI/AAAAAAAAGxA/-LFn7yhH31QtuHa-RmPRvlNruS9IAIbZwCLcBGAsYHQ/s3303/Canarytokens-Logo-01.png)
+Python program that scrapes files to detect honey-tokens.
+For now, only canary-tokens are fingerprinted. Future work can include more types
--- master_script.py
+++ master_script.py
@@ -1,12 +1,52 @@
 import os
 import subprocess
 
+
 def get_type(file):
     print("-------- GET TYPE  --------")
     file_name, file_extension = os.path.splitext(file)
     print(f"Filename: {file_name}")
     print(f"File extension: {file_extension}")
-    return(file_extension)
+    return file_extension
+
+
+def is_kubeconfig_file(file):
+    kubeconf_keywords = ["apiVersion", "kind", "clusters",
+                         "users", "contexts", "current-context"]
+    file_content = open(file).read()
+    if file_content.__contains__(kubeconf_keywords[0]) \
+            and file_content.__contains__(kubeconf_keywords[1]) \
+            and file_content.__contains__(kubeconf_keywords[2]) \
+            and file_content.__contains__(kubeconf_keywords[3]) \
+            and file_content.__contains__(kubeconf_keywords[4]) \
+            and file_content.__contains__(kubeconf_keywords[5]):
+        return True
+
+
+def is_wireguard_file(file):
+    wireguard_keywords = ["[Interface]", "PrivateKey", "Address",
+                          "[Peer]", "PublicKey", "AllowedIPs", "Endpoint", "PersistentKeepalive"]
+    file_content = open(file).read()
+    if file_content.__contains__(wireguard_keywords[0]) \
+            and file_content.__contains__(wireguard_keywords[1]) \
+            and file_content.__contains__(wireguard_keywords[2]) \
+            and file_content.__contains__(wireguard_keywords[3]) \
+            and file_content.__contains__(wireguard_keywords[4]) \
+            and file_content.__contains__(wireguard_keywords[5]) \
+            and file_content.__contains__(wireguard_keywords[6]) \
+            and file_content.__contains__(wireguard_keywords[7]):
+        print("This is a Wireguard config file")
+        return True
+
+
+def run_process(file, string_to_search, script):
+    p = subprocess.run([f"python3 {script} -f {file} -s {string_to_search}"],
+                       shell=True, capture_output=True, text=True)
+    print(f"Return code: {p.returncode}")
+    print(f"Stdout:\n{p.stdout}")
+    if p.returncode == 0 and "detected" in p.stdout:
+        print(f"{os.path.join(os.getcwd(), file)} is a honeytoken.")
+
 
 def main():
     print("-------- START SCRIPT  --------")
@@ -16,31 +56,37 @@ def main():
     else:
         print("File not found. Check the path.")
         exit(1)
-    
+
     type = get_type(file)
     print("-------- CASE  --------")
-    match type:
-        case ".txt":
-            print("Text file found.")
-        case ".pdf":
-            print("It's a damn pdf bro.")
-        case ".xlsx":
-            print("Excel document found.")
-        case ".docx":
-            print("Word document found.")
-            p = subprocess.run([f"python3.10 docx.py -f {file} -s {string_to_search}"],
-                               shell=True, capture_output=True, text=True)
-        case ".sql":
-            print("SQL file found.")
-        case ".conf":
-            print("Configuration file found.")
-        case _:
-            print("No file extension.")
+    if ".txt" in type:
+        print("Text file found.")
 
-    print(f"Return code: {p.returncode}")
-    print(f"Stdout:\n{p.stdout}")
-    if p.returncode == 0 and "detected" in p.stdout:
-        print(f"{os.path.join(os.getcwd(), file)} is a honeytoken.")
+    elif ".pdf" in type:
+        print("Pdf file found.")
+
+    elif ".xlsx" in type:
+        print("Excel file found.")
+
+    elif ".docx" in type:
+        print("Word document found.")
+        run_process(file, string_to_search, "docx.py")
+
+    elif ".sql" in type:
+        print("SQL file found.")
+    #     ask the administration to keep the windows open ( or at least make a button to control each window)
+
+    elif ".conf" in type:
+        print("Configuration file found.")
+
+    elif "_" in type or "" in type:
+        print("No file extension.")
+        if is_wireguard_file(file):
+            run_process(file, string_to_search, "wg.py")
+        if is_kubeconfig_file(file):
+            print("Kubernetes config file detected")
+            run_process(file, string_to_search, "kube.py")
+
 
 if __name__ == "__main__":
     main()
