6f24db978f566ac3b73e9ef0e5da7c6b3d74d648
mman21 <mman21@student.aau.dk>
2022-03-04 14:01:25 +0100

    Office_docs updated to fit new amster_script

--- office_docx.py
+++ office_docx.py
@@ -3,15 +3,14 @@ import zipfile
 import re
 from tokenfinder import Tokenfinder
 
-def check_office_files():
+def check_office_files(file_location):
     # Parsing arguments from master_script.py
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--file", "-f", type=str, required=True)
+    
     parser.add_argument("--string", "-s", type=str, required=True)
-    args = parser.parse_args()
+    
 
-    # Boolean to determine if anything is found
-    found = 0
+    # List of urls found
+    list_of_urls = []
     try:
         # Unzip the office file without saving to folder
         unzipped_file = zipfile.ZipFile(args.file,"r")
@@ -20,20 +19,24 @@ def check_office_files():
         # Reads every file in the zip file and looks if it contains the string you wish to search for
         for item in namelist:
             content = str(unzipped_file.read(item))
-            tokenlist = re.findall(Tokenfinder.pattern, content)
-            for token in tokenlist:
-                for t in token:
-                    if Tokenfinder.find_canarytoken(t):
-                        print("Canarytoken found in: ", item)
-                        print(t)
-                        print()
-                        found += 1
+            token = Tokenfinder.find_tokens_in_string(content)
+            if token:
+                list_of_urls.extend(token)
     except OSError:
         print("error")
     
     # If no results of the search
-    if found == 0:
-        print("No canarytoken found")
+    if len(list_of_urls) == 0:
+        return None
+    else:
+        print(list_of_urls)
+        return list_of_urls
 
 if __name__ == "__main__":
-    check_office_files()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--file", "-f", type=str, required=True)
+    args = parser.parse_args()
+    
+    file_location = args.file
+
+    check_office_files(file_location)
