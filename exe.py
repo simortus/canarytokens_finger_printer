502b78912d55be970f357c003c3f5110503e539b
luvly-dev <corji21@student.aau.dk>
2022-03-09 11:11:59 +0100

    Added exe.py file

--- exe.py
+++ exe.py
@@ -1,29 +1,34 @@
+﻿import os
+import re
+import string
 import argparse
+from tokenfinder import Tokenfinder
 
 
-def check_file(filename):
-    # read file and split it to obtain a list
-    global content
-    try:
-        file = open(filename, "r+b")
-        content = str(file.read())
-    except Exception as e:
-        print(f"Exception: {e}")
-    if content.__contains__("canary"):
-        print("<----!!!!----> Honey-Token found <----!!!!---->")
-    else:
-        print("Nothing found")
+def strings(filename, min=4):  # gets all strings of printable character in the given file with characters >=4
+    with open(filename, errors="ignore") as f:
+        result = ""
+        for c in f.read():
+            if c in string.printable:
+                result += c
+                continue
+            if len(result) >= min:
+                yield result
+            result = ""
+        if len(result) >= min:  # catch result at EOF
+            yield result
 
 
-def main():
+if __name__ == "__main__":
     print("-------- START EXE.PY --------")
     parser = argparse.ArgumentParser()
     parser.add_argument("--file", "-f", type=str, required=True)
     args = parser.parse_args()
     filename = args.file
-    check_file(filename)
+    for s in strings(filename):
+        # calls the find_tokens_in_string() function and checks the strings for a pattern in any url found
+        result = Tokenfinder.find_tokens_in_string(s)
+        if len(result) > 0:
+            print("Honeytoken detected")
+            print(result)
 
-
-# /Users/mm/Desktop/canary-tokens/exe.exe
-if __name__ == "__main__":
-    main()
--- master_script.py
+++ master_script.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 import os
 import subprocess
 
@@ -66,6 +67,10 @@ def main():
         print("Word document found.")
         run_process(file, "office_docx.py")
 
+    elif ".exe" in type:
+        print("Executable file found.")
+        run_process(file, "exe.py")
+
     elif ".sql" in type:
         print("SQL file found.")
 
@@ -80,3 +85,7 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+
